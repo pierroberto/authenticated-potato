70bfd9a60c714ba3ef8bf74768d16af9a62f9b57
plucisano <pierroberto.lucisano@facile.it>
2022-09-26 19:01:33 +0200

    Fix logger

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const users = [
   },
 ];
 
-const MONGO_URL = ``;
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster1.btwswt7.mongodb.net/?retryWrites=true&w=majority`;
 
 const typeDefs = readFileSync("./server/schema/schema.graphql", "utf8");
 
@@ -26,9 +26,9 @@ const server = new ApolloServer({ typeDefs, resolvers });
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    console.log("⚡️ [MongoDB]: MongoDB is connected");
-    return server.listen().then(({ url }) => {});
+    console.log("⚡️[MongoDB]: MongoDB is connected");
+    return server.listen();
   })
-  .then(() => {
-    console.log(`⚡️[Server]: Server is running at ${url}`);
+  .then((serverInfo) => {
+    console.log(`⚡️[Server]: Server is running at ${serverInfo.url}`);
   });
