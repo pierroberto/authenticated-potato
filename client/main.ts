75bd2842234ad76b97c3afef3b5baba01c0b1bbb
plucisano <pierroberto.lucisano@facile.it>
2022-10-01 13:22:38 +0200

    Remove imports + add mock query

--- client/main.ts
+++ client/main.ts
@@ -1,28 +1,42 @@
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
 const tryLogin = () => {
   const maybeUsername = document.getElementById('username')
   const maybePassword = document.getElementById('password')
 
   if (maybeUsername === null || maybePassword === null) return
 
-  const baseUrl = process.env.BASE_URL || window.location.href
+  const baseUrl = window.location.href
 
-  fetch(`${baseUrl}/graphql`, {
+  fetch(`${baseUrl}graphql`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query: `{ 
-        user: { username } 
-      }`,
+      query: `
+                query Query($username: String!, $password: String!) {
+                  user(username: $username, password: $password) {
+                    username
+                  }
+                }
+              `,
+      variables: {
+        username: 'admin',
+        password: 'admin',
+      },
     }),
   })
     .then((response) => response.json())
     .then((response) => response)
+}
+
+const submitLoginForm = (event: Event) => {
+  console.log(event)
+  event.preventDefault()
+
+  if (event.target === null) return
+
+  //console.log(event.target['username'].value)
+  //console.log(event.target['password'].value)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
